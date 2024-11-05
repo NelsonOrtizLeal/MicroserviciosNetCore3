06ec48c69014e265b2f11c1148f4a5d8b234e3ff
Nelson Ortiz Leal <114643735+NelsonOrtizLeal@users.noreply.github.com>
2024-11-05 16:21:12 -0600

    Add: Agregando controlador '/'

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroserviciosNetCore3.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Running ...";
+        }
+    }
+}
