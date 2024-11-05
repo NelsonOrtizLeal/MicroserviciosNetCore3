5ff3ead8411390f276adfcfee4e71b4ce435cbf8
Nelson Ortiz Leal <114643735+NelsonOrtizLeal@users.noreply.github.com>
2024-11-05 17:20:20 -0600

    Update: propiedades del producto requeridas

--- ProductService.cs
+++ ProductService.cs
@@ -1,12 +1,15 @@
-﻿using System.Security.Policy;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Policy;
 
 namespace MicroserviciosNetCore3
 {
     public class Product
     {
         public int Id { get; set; }
+        [Required] // DataNotation
         public string Name { get; set; }
         public decimal Price { get; set; }
+        [Required]
         public string Description { get; set; }
     }
 }
